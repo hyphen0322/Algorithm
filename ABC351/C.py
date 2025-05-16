bf129b69f725d33e8938e7ea3965adf3dff37784
hyphen0322 <kotone.suzuki12@gmail.com>
2025-05-16 13:52:34 +0900

    make C

--- ABC351/C.py
+++ ABC351/C.py
@@ -1,9 +1,19 @@
 N = int(input())
 A = list(map(int, input().split()))
-for i in range(N):
-    if A[-1] == A[-2]:
-        sum = A[-1] + A[-2]
-        A.pop(-1)
-        A.pop(-1)
-        A.append(sum)
+balls = []
 
+for i in range(N):
+    # ボールの大きさは 2^A[i]
+    balls.append(2 ** A[i])
+    
+    # 連結できる限り繰り返す
+    while len(balls) > 1 and balls[-1] == balls[-2]:
+        # 2つのボールを合成
+        new_value = balls[-1] + balls[-2]
+        # 2つのボールを取り除く
+        balls.pop()
+        balls.pop()
+        # 合成したボールを追加
+        balls.append(new_value)
+
+print(len(balls))
